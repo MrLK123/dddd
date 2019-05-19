8c78e4f9aa0d5184fd51869f4b4eefe6f98ae663
刘嘿哈 <mr_lk123@163.com>
2019-05-19 18:33:57 +0800

    add reset.css

---
+++ src/pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Home
+        <router-link to="/list">List</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/list/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        List
+  
+       <router-link to="/">首页</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"List"
+}
+</script>
+
+<style scoped>
+
+</style>
