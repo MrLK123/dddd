c62e993e44910a71541ae15e7e041e45b949a933
刘嘿哈 <mr_lk123@163.com>
2019-05-22 09:33:02 +0800

    add seach to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-seach></city-seach>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySeach from './components/Seach'
     export default {
         name:'City',
-        components:{CityHeader}
+        components:{CityHeader,CitySeach}
     }
 </script>
 
---
+++ src/pages/city/components/Seach.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="seach">
+        <input type="text" class="seach-input" placeholder="输入城市名称或拼音"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CitySeach"
+    }
+</script>
+
+<style  lang='stylus' scoped>
+@import '~styles/varibles'
+  .seach
+    padding:.1rem
+    line-height: .72rem
+    background: $bgColor
+    color:#666
+    .seach-input
+        width:100%
+        height 0.62rem
+        line-height: .62rem
+        text-align:center
+        border-radius:.06rem
+        box-sizing:border-box
+        padding:0 .2rem
+</style>
+
+
